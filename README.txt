93e45e31b5a5c70d41c96c52ea6eb910b6fb4c4f
Abhijeet Jadhav <ethosystem@gmail.com>
2019-04-20 08:16:07 +0530

    Test POST /todos route

---
+++ README.txt
@@ -0,0 +1,5 @@
+USE THE TESTINT TOOL LIKE 
+
+npm i expect mocha nodemon supertest --save-dev
+
+IT is save as dev dependency only
--- server/server.js
+++ server/server.js
@@ -18,10 +18,12 @@ app.post('/todos', (req, res) => {
 	todo.save().then((docs) => {
 		res.send(docs);
 	}, (err) => {
-		res.send(err);
+		res.status(400).send(err);
 	});
 });
 
 app.listen(3000, () => {
 	console.log('Started on port 3000');
 });
+
+module.exports = {app};
