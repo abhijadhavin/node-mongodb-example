3a3b1dec933e7f971380a28d20962edfe81e0b48
Abhijeet Jadhav <ethosystem@gmail.com>
2019-04-22 21:56:05 +0530

    Add Delete and patch test case

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
 	'text': 'First test todo'
 }, {
 	_id: new ObjectID(),
-	text: 'second test todo'
+	text: 'second test todo',
+	complated: true,
+	completedAt: 3333
 }];
 
 beforeEach((done) => {
@@ -103,5 +105,89 @@ describe("GET /todos/:id", () => {
 	}); 
 });
 
+describe("DELETE /todos/:id", () => {
+	it('should remove a todo', (done) => {
+		var hexId = todos[0]._id.toHexString();
+		request(app)
+			.delete(`/todos/${hexId}`)
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo._id).toBe(hexId);
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err)
+				}
+
+				Todo.findById(hexId).then((todo) => {
+					expect(todo).toBeNull();
+					done();
+				}).catch((e) => done(e));
+			});
+	});
+
+	it('should return 404 if todo not found', (done) => {
+		var hexId = new ObjectID().toHexString();
+
+		request(app)
+			.get(`/todos/${hexId}`)
+			.expect(404)
+			.end(done);
+	});
+
+	it('should return 404 if object not found', (done) => {
+		var hexId = new ObjectID().toHexString();
+		request(app)
+			.get(`/todos/116sdas`)
+			.expect(400)
+			.end(done);	
+	})
+});	
+
+
+
+
+describe('PATCH /todos/:id', () => {
+
+	it('should update the todo', (done) => {
+		//grab id of first item
+		var hexId = todos[0]._id.toHexString();
+		var text = "This should be the new text"
+		 
+		request(app)
+			.patch(`/todos/${hexId}`)
+			.send({
+				complated: true,
+				text
+			})
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.text).toBe(text);
+				expect(res.body.todo.complated).toBe(true);
+				expect(res.body.todo.completedAt).toBeGreaterThan(0);
+			})
+	 		.end(done);
+
+	});
+
+	it('should clear complated when todo is not complated', (done) => {
+		var hexId = todos[0]._id.toHexString();
+		var text = "This should be the new text!!!"
+		 
+		request(app)
+			.patch(`/todos/${hexId}`)
+			.send({
+				complated: false,
+				text
+			})
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.text).toBe(text);
+				expect(res.body.todo.complated).toBe(false);
+				expect(res.body.todo.completedAt).toBeNull();
+			})
+	 		.end(done);
+	});	
+});
 
 
